1ff7104a577fe0e6a5e0dfcab3d9c8805b7c7f96
Yongho Kim <winthisgame12@gmail.com>
2023-05-20 00:31:47 -0400

    add CSS in Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from './components/Navbar';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <>
      <Navbar />
+     <Outlet />
     </>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{jsx,js}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#F96162',
+      },
+    },
+  },
+  plugins: [],
+}
+
